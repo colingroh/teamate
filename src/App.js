0e5ab26c05eeb45f8828e84e8dda8f6bcfc9a90e
Ralph Dela Cruz <ralphdelacruz@ralphs-mbp.myfiosgateway.com>
2020-11-23 01:18:28 -0500

    added dashboard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import Preferences from './Preferences';
+import Dashboard from './Dashboard'
 
 function App() {
   return (
     <div className="App">
-      <Preferences />
+        <Dashboard />
     </div>
   );
 }
