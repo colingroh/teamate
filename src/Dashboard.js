0a504581244376874ab88b3e53dbabc6366d48eb
colingroh <33656919+colingroh@users.noreply.github.com>
2020-11-23 13:24:13 -0500

    added-scrollin

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -58,20 +58,20 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function Dashboard(props) {
-    function useLockBodyScroll() {
-        useLayoutEffect(() => {
-         // Get original body overflow
-         const originalStyle = window.getComputedStyle(document.body).overflow;  
-         // Prevent scrolling on mount
-         document.body.style.overflow = 'hidden';
-         // Re-enable scrolling when component unmounts
-         return () => document.body.style.overflow = originalStyle;
-         }, []); // Empty array ensures effect is only run on mount and unmount
-      }
+    // function useLockBodyScroll() {
+    //     useLayoutEffect(() => {
+    //      // Get original body overflow
+    //      const originalStyle = window.getComputedStyle(document.body).overflow;  
+    //      // Prevent scrolling on mount
+    //      document.body.style.overflow = 'hidden';
+    //      // Re-enable scrolling when component unmounts
+    //      return () => document.body.style.overflow = originalStyle;
+    //      }, []); // Empty array ensures effect is only run on mount and unmount
+    //   }
 
     const classes = useStyles();
 
-    useLockBodyScroll();
+    //useLockBodyScroll();
 
   function FormRow1() {
     return (
