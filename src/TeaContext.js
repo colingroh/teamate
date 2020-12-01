fe87bc18c6e04a820bca5608a546e86d74f20eac
colingroh <33656919+colingroh@users.noreply.github.com>
2020-12-01 11:20:55 -0500

    tiny edit

--- src/TeaContext.js
+++ src/TeaContext.js
@@ -4,9 +4,9 @@ export const TeaContext = React.createContext({});
 
 //TeaMate or Party, submittedSearch, selectedCardID,
 export function TeaProvider({ children }) {
-  const [searchType, setSearchType] = React.useState("");
-  const [submittedSearch, setSubmittedSearch] = React.useState("");
-  const [selectedCardID, setSelectedCardID] = React.useState(null);
+  const [searchType, setSearchType] = React.useState(""); //Can be one of "TeaMate" or "TeaParty"
+  const [submittedSearch, setSubmittedSearch] = React.useState(""); //Can be one of "TeaMate" or "TeaParty"(set when search button is clicked)
+  const [selectedCardID, setSelectedCardID] = React.useState(null); //null when unselected, otherwise set to some ID we assign to the match cards. 
 
   return (
     <TeaContext.Provider
